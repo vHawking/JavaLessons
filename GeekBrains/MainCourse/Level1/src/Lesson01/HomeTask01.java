e8227e57376e383f905bfd85257322e2d5b45b68
vHawking <mailcome@yandex.ru>
2018-03-27 02:23:25 +0300

    Rebuild all HomeTask01

--- GeekBrains/MainCourse/Level1/src/Lesson01/HomeTask01.java
+++ GeekBrains/MainCourse/Level1/src/Lesson01/HomeTask01.java
@@ -8,6 +8,61 @@ package Lesson01;
  */
 
 public class HomeTask01 {
+
+/*
+    3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат, где a, b, c, d – входные
+    параметры этого метода;
+*/
+
+    private static int calculate(int a, int b, int c, int d) {
+        return a * (b + (c / d));
+    }
+
+/*
+    4. Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах
+    от 10 до 20 (включительно), если да – вернуть true, в противном случае – false;
+*/
+
+    private static boolean between10and20(int x, int y) {
+        int sum = x + y;
+        return sum >= 10 && sum <= 20;
+    }
+
+/*
+    5. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль
+    положительное ли число передали, или отрицательное. Замечание: ноль считаем положительным числом;
+*/
+
+    private static boolean positiveOrNegative(int num) {
+        return num >= 0;
+    }
+
+/*
+    6. Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true,
+    если число отрицательное;
+*/
+
+    private static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+/*
+    7. Написать метод, которому в качестве параметра передается строка, обозначающая имя, метод должен
+    вывести в консоль сообщение «Привет, указанное_имя!»;
+*/
+
+    private static String Hello(String name) {
+        return "Привет, " + name;
+    }
+
+    /*
+        8. Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль.
+        Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+    */
+    private static boolean isLeapYearBool(int x) {
+        return x % 4 == 0 && x % 100 != 0 || x % 400 == 0;
+    }
+
     public static void main(String[] args) {
 
 /*
@@ -48,29 +103,31 @@ public class HomeTask01 {
         // Задание 4
         System.out.println("Задание 4.\n");
 
-        int x = 5;
+        int x = 10;
         int y = 13;
         System.out.println("Значения инициализированных переменных:");
         System.out.println("x = " + x + "; " + "y = " + y + ";");
         System.out.printf("Сумма заданных значений равна %d. ", x + y);
-        System.out.printf("Метод возвращает значение %s.\n\n", between10and20(x, y) ? "true" : "false");
+        System.out.printf("Метод возвращает значение %s.\n\n", between10and20(x, y));
 
         // Задание 5
         System.out.println("Задание 5.\n");
 
-        positiveOrNegative(300000);
+        int num = 32768;
+        System.out.printf("Значение переменной = %d. ", num);
+        System.out.printf("Число %s.\n\n", positiveOrNegative(num) ? "положительное" : "отрицательное");
 
         // Задание 6
         System.out.println("Задание 6.\n");
 
-        boolean tf2 = isNegative(-13);
-        System.out.println("Метод возвращает значение " + tf2 + ";\n");
+        int number = -13;
+        System.out.printf("Значение переменной = %d. Метод возвращает значение %s.\n\n", number, isNegative(number));
 
         // Задание 7
         System.out.println("Задание 7.\n");
 
-        String str2 = Hello("Деннис Ритчи");
-        System.out.println(str2 + "!\n");
+        String name = "Деннис Ритчи";
+        System.out.println(Hello(name) + "!\n");
 
         // Задание 8
         System.out.println("Задание 8.\n");
@@ -82,68 +139,8 @@ public class HomeTask01 {
                 "делится на 4 без остатка, но при делении на 400 получается остаток. Значит год невисокосный.\n");
 
         int x1 = 2016;
-        System.out.printf("Задан %d год. Он %s является високосным.\n", x, (isLeapYearBool(x1)) ? "\b" : "не");
-    }
-
-/*
-    3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат, где a, b, c, d – входные
-    параметры этого метода;
-*/
-
-    private static int calculate(int a, int b, int c, int d) {
-        return a * (b + (c / d));
-    }
-
-/*
-    4. Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах
-    от 10 до 20 (включительно), если да – вернуть true, в противном случае – false;
-*/
-
-    private static boolean between10and20(int x, int y) {
-        int sum = x + y;
-        return sum >= 10 && sum <= 20;
-    }
-
-/*
-    5. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль
-    положительное ли число передали, или отрицательное. Замечание: ноль считаем положительным числом;
-*/
-
-    private static void positiveOrNegative(int x) {
-        System.out.println("Значение заданной переменной int x = " + x + ";");
-
-        if (x >= 0) {
-            System.out.println("Число положительное.\n");
-        } else {
-            System.out.println("Число отрицательное.\n");
-        }
-    }
-
-/*
-    6. Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true,
-    если число отрицательное;
-*/
-
-    private static boolean isNegative(int number) {
-        System.out.println("Значение заданной переменной int number = " + number + ";");
-
-        return number < 0;
-    }
-
-/*
-    7. Написать метод, которому в качестве параметра передается строка, обозначающая имя, метод должен
-    вывести в консоль сообщение «Привет, указанное_имя!»;
-*/
-
-    private static String Hello(String name) {
-        return "Привет, " + name;
-    }
-
-/*
-    8. Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль.
-    Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-*/
-    private static boolean isLeapYearBool(int x) {
-        return x % 4 == 0 && x % 100 != 0 || x % 400 == 0;
+        System.out.printf("Задан %d год. Он %s является високосным.\n", x1, (isLeapYearBool(x1)) ? "\b" : "не");
+        int x2 = 2018;
+        System.out.printf("Задан %d год. Он %s является високосным.\n", x2, (isLeapYearBool(x2)) ? "\b" : "не");
     }
 }
