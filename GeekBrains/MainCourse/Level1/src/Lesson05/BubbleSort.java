b48e280cb80a6b2b1ae463a427034a012df3c76f
Vadim Yastrebov <mailcome@yandex.ru>
2018-03-28 01:40:03 +0300

    Just continue fixing errors

--- GeekBrains/MainCourse/Level1/src/Lesson05/BubbleSort.java
+++ GeekBrains/MainCourse/Level1/src/Lesson05/BubbleSort.java
@@ -91,7 +91,6 @@ public class BubbleSort {
             for (int i = 0; i < arr.length - 1; i++) {
                 if (arr[i] > arr[i + 1]) {
                     arrSorted = false;
-
                     int buf = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = buf;
