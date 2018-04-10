9c3982c5015cfe760e20d0b4d6c0797fb697853d
Vadim Yastrebov <mailcome@yandex.ru>
2018-04-10 17:19:31 +0300

    Removed some spaces

--- GeekBrains/Level1/src/Lesson07/MainClassString.java
+++ GeekBrains/Level1/src/Lesson07/MainClassString.java
@@ -10,12 +10,10 @@ package Lesson07;
 
 public class MainClassString {
     public static void main(String[] args) {
-
         String str1 = "   Предложение один    Теперь предложение два     Предложение три     А тут предложение четыре     ";
         System.out.println("\nБыло:\n" + str1);
 
         String str2 = str1.trim().replaceAll(" +", " ");
-
         StringBuilder stringBuilder = new StringBuilder(str2);
 
         for (int i = str2.length() - 1; i >= 1; i--) {
