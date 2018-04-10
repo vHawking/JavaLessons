d85720463b2d7e4120814defc80d0aa5fc23e1a8
Vadim Yastrebov <mailcome@yandex.ru>
2018-04-10 17:11:03 +0300

    Some minor fixes

--- GeekBrains/Level1/src/Lesson07/MainClassString.java
+++ GeekBrains/Level1/src/Lesson07/MainClassString.java
@@ -2,11 +2,10 @@ package Lesson07;
 
 /**
  * Java. Уровень1. Дополнительное домашнее задание по 7 лекции.
+ * Привести строку к нормальному виду.
  *
  * @author Вадим Ястребов.
  * @version 18 Февраля 2018 г.
- *
- * Привести строку к нормальному виду.
  */
 
 public class MainClassString {
@@ -19,9 +18,9 @@ public class MainClassString {
 
         StringBuilder stringBuilder = new StringBuilder(str2);
 
-        for(int i = str2.length()-1; i >= 1; i--) {
-            if(str2.charAt(i) >= 'A' && str2.charAt(i) <= 'Я') {
-                stringBuilder.insert(i-1, ".");
+        for (int i = str2.length() - 1; i >= 1; i--) {
+            if (str2.charAt(i) >= 'A' && str2.charAt(i) <= 'Я') {
+                stringBuilder.insert(i - 1, ".");
             }
         }
         System.out.println("\nСтало:\n" + stringBuilder.append('.').toString());
